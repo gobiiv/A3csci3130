e4bd1dd8e3cff93f7770b7765c6e78b87f7ec8a8
gobiiv <39447197+gobiiv@users.noreply.github.com>
2018-06-30 16:17:52 -0300

    Updated 4 files
    updated the following files:
    1) Contact.java
    updated the fields/ constructors
    updated toMap method to reflect new fields
    
    2) CreateContactActivity.java
    added new textview and spinner
    added onItemSelected and onNothingSelected methods for spinners
    updated submitInfoButton to reflect new fields
    
    3) activity_create_contact_activity.xml
    added new elements to activity
    linked string array to spinners
    changed ids for new elements
    
    4) strings.xml
    added 2 string arrays for 2 spinners
    1 string array for primary_business spinner
    1 string array for province spinner
    
    - GOBII VIVAGANANDA

--- app/src/main/java/com/acme/a3csci3130/CreateContactAcitivity.java
+++ app/src/main/java/com/acme/a3csci3130/CreateContactAcitivity.java
@@ -3,17 +3,23 @@ package com.acme.a3csci3130;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
 
-public class CreateContactAcitivity extends Activity {
+public class CreateContactAcitivity extends Activity implements AdapterView
+        .OnItemSelectedListener {
 
     private Button submitButton;
-    private EditText nameField, emailField;
+    private EditText nameField, business_numberField, addressField;
+    private Spinner primary_businessField, provinceField;
     private MyApplicationData appState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_contact_acitivity);
         //Get the app wide shared variables
@@ -21,17 +27,37 @@ public class CreateContactAcitivity extends Activity {
 
         submitButton = (Button) findViewById(R.id.submitButton);
         nameField = (EditText) findViewById(R.id.name);
-        emailField = (EditText) findViewById(R.id.email);
+        business_numberField = (EditText) findViewById(R.id.business_number);
+        primary_businessField = (Spinner) findViewById(R.id.primary_business);
+        //primary_businessField.setOnItemSelectedListener(this);
+        addressField = (EditText) findViewById(R.id.address);
+        provinceField = (Spinner) findViewById(R.id.province);
+        //provinceField.setOnItemSelectedListener(this);
+
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
     }
 
     public void submitInfoButton(View v) {
         //each entry needs a unique ID
-        String personID = appState.firebaseReference.push().getKey();
-        String name = nameField.getText().toString();
-        String email = emailField.getText().toString();
-        Contact person = new Contact(personID, name, email);
+        String uid = appState.firebaseReference.push().getKey();
+		String business_number = business_numberField.getText().toString();
+		String name = nameField.getText().toString();
+        String primary_business = primary_businessField.getSelectedItem().toString();
+        String address = addressField.getText().toString();
+        String province = provinceField.getSelectedItem().toString();
 
-        appState.firebaseReference.child(personID).setValue(person);
+        Contact person = new Contact(uid, business_number, name, primary_business, address, province);
+
+        appState.firebaseReference.child(uid).setValue(person);
 
         finish();
 
--- app/src/main/java/com/acme/a3csci3130/DetailViewActivity.java
+++ app/src/main/java/com/acme/a3csci3130/DetailViewActivity.java
@@ -2,9 +2,7 @@ package com.acme.a3csci3130;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 public class DetailViewActivity extends Activity {
@@ -18,8 +16,8 @@ public class DetailViewActivity extends Activity {
         setContentView(R.layout.activity_detail_view);
         receivedPersonInfo = (Contact)getIntent().getSerializableExtra("Contact");
 
-        nameField = (EditText) findViewById(R.id.name);
-        emailField = (EditText) findViewById(R.id.email);
+        nameField = (EditText) findViewById(R.id.business_number);
+        emailField = (EditText) findViewById(R.id.name);
 
         if(receivedPersonInfo != null){
             nameField.setText(receivedPersonInfo.name);
