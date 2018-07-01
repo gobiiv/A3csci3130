192b8e8e33601fa8daaf14ce60b99c4357e88293
gobiiv <39447197+gobiiv@users.noreply.github.com>
2018-06-30 22:55:47 -0300

    final
    refactored and commented version for all files.

---
+++ app/src/androidTest/java/com/acme/a3csci3130/CRUDEspressoTest.java
@@ -0,0 +1,9 @@
+/*
+This is the espresso UI test for the CRUD methods.
+ */
+package com.acme.a3csci3130;
+
+public class CRUDEspressoTest {
+
+
+}
---
+++ app/src/test/java/com/acme/a3csci3130/CRUDUnitTest.java
@@ -0,0 +1,12 @@
+/*
+This is the unit test for the CRUD methods.
+ */
+package com.acme.a3csci3130;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CRUDUnitTest {
+
+}
